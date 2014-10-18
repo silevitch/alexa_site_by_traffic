b40c01120cf6af6864d9568f6013caa77d71581e
Paul Silevitch <silevitch@users.noreply.github.com>
2014-10-18 08:01:16 -0400

    more polish

--- ranking.pl
+++ ranking.pl
@@ -5,29 +5,38 @@ use warnings;
 
 use LWP::Simple;
 
-my $monthly_visitors = $ARGV[0];
+use constant  {
+	RANKINGS_PER_PAGE	=> 25,
+	MAX_RANKING		=> 500,
+	RANKINGS_URL		=> 'http://www.alexa.com/topsites/global;',
+	COEFFICIENT		=> 104_943_144_672,
+	POWER			=> 1 / -1.008,
+};
 
-# We need to do the inverse of the formula given on http://netberry.co.uk/alexa-rank-explained.htm:
-# $monthly_visitors = 104_943_144_672 * ( $ranking ^ -1.008)
-#
-my $ranking = int ( ( $monthly_visitors / 104_943_144_672 ) ** ( 1 / -1.008 ) );
+my $monthly_visitors = $ARGV[0] || die "Please pass in a monthly visitor amount";
 
-my $page = int ( $ranking / 25 );
+# We need to do the inverse of the formula given on http://netberry.co.uk/alexa-rank-explained.htm
+my $ranking = int ( ( $monthly_visitors / COEFFICIENT ) ** ( POWER ) );
+
+die "Ranking is not in the top " . MAX_RANKING if ( $ranking >= MAX_RANKING );
+
+my $page = int ( $ranking / RANKINGS_PER_PAGE );
 
 my @sites = scrape( $page );
 
-if ( int ( $ranking % 25 ) == 0 ){
+# need to handle the case where we need to scrape another page
+if ( $ranking % RANKINGS_PER_PAGE == 0 ){
 	push @sites, scrape( $page + 1 );
 }
 
-for my $i ( $ranking .. $ranking + 1 ) {
-	printf("%d:\t%s\n", $i, $sites[ $i % 25 ] );
+for my $slot ( $ranking .. $ranking + 1 ) {
+	printf("%d:\t%s\n", $slot, $sites[ $slot % RANKINGS_PER_PAGE ] );
 }
 
 sub scrape {
 	my $page = shift || 0;
 
-	my $html = get('http://www.alexa.com/topsites/global;' . $page) or die "Could not scrape: $!";
+	my $html = get( RANKINGS_URL . $page ) or die "Could not scrape $page: $!";
 
 	my @sites = $html =~ m{<a href="/siteinfo/([^"]+)">}g;
 
