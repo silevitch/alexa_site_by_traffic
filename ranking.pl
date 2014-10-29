ade9b29f321c48b8cff321c013da528fb5cc4e6f
silevitch <silevitch@users.noreply.github.com>
2014-10-29 15:31:21 -0400

    Ran through perltidy & perlcritic (--brual) and fixed EVERYTHING

--- ranking.pl
+++ ranking.pl
@@ -3,43 +3,47 @@
 use strict;
 use warnings;
 
+use version; our $VERSION = qv('1.0.0');
+
 use LWP::Simple;
+use Carp qw(croak);
+use Readonly;
 
-use constant  {
-	RANKINGS_PER_PAGE	=> 25,
-	MAX_RANKING		=> 500,
-	RANKINGS_URL		=> 'http://www.alexa.com/topsites/global;',
-	COEFFICIENT		=> 104_943_144_672,
-	POWER			=> 1 / -1.008,
-};
+Readonly my $RANKINGS_PER_PAGE => 25;
+Readonly my $MAX_RANKING       => 500;
+Readonly my $RANKINGS_URL      => 'http://www.alexa.com/topsites/global;';
+Readonly my $COEFFICIENT       => 104_943_144_672;
+Readonly my $POWER             => 1 / -1.008;
 
-my $monthly_visitors = $ARGV[0] || die 'Please pass in a monthly visitor amount';
+my $monthly_visitors = $ARGV[0]
+  || croak 'Please pass in a monthly visitor amount';
 
 # We need to do the inverse of the formula given on http://netberry.co.uk/alexa-rank-explained.htm
-my $ranking = int ( ( $monthly_visitors / COEFFICIENT ) ** ( POWER ) );
+my $ranking = int( ( $monthly_visitors / $COEFFICIENT )**($POWER) );
 
-die 'Ranking is not in the top ' . MAX_RANKING if ( $ranking >= MAX_RANKING );
+croak 'Ranking is not in the top ' . $MAX_RANKING
+  if ( $ranking >= $MAX_RANKING );
 
-my $page = int ( $ranking / RANKINGS_PER_PAGE );
+my $page = int( $ranking / $RANKINGS_PER_PAGE );
 
-my @sites = scrape( $page );
+my @sites = scrape($page);
 
 # need to handle the case where we need to scrape another page
-if ( $ranking % RANKINGS_PER_PAGE == 0 ){
-	push @sites, scrape( $page + 1 );
+if ( $ranking % $RANKINGS_PER_PAGE == 0 ) {
+    push @sites, scrape( $page + 1 );
 }
 
 for my $slot ( $ranking .. $ranking + 1 ) {
-	printf("%d:\t%s\n", $slot, $sites[ $slot % RANKINGS_PER_PAGE ] );
+    printf "%d:\t%s\n", $slot, $sites[ $slot % $RANKINGS_PER_PAGE ];
 }
 
 sub scrape {
-	my $page = shift || 0;
+    my $number = shift || 0;
 
-	my $html = get( RANKINGS_URL . $page ) or die "Could not scrape $page: $!";
+    my $html = get( $RANKINGS_URL . $number )
+      or croak "Could not scrape page $number";
 
-	my @sites = $html =~ m{<a href="/siteinfo/([^"]+)">}g;
+    my @found_sites = $html =~ m{<a href="/siteinfo/([^"]+)">}xmsg;
 
-	return @sites;
+    return @found_sites;
 }
-
